30036922dff71283db2597ae9175c83b488158ec
ChanYong Park <krap5075@naver.com>
2023-11-20 06:22:39 +0900

    style: error색상 추가

--- frontend/project/src/styles/theme.js
+++ frontend/project/src/styles/theme.js
@@ -3,10 +3,15 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#008000'
+      main: '#008000',
+      light: '#4caf50'
     },
     secondary: {
-      main: '#303F9F'
+      main: '#303F9F',
+      light: '#0288d1'
+    },
+    error: {
+      main: '#d32f2f'
     }
   },
   typography: {
