87d2a48073a4815a259f36eb5f45b0b7da4e8fd4
ChanYong Park <krap5075@naver.com>
2023-11-22 17:10:03 +0900

    feat(Card): 카드 컴포넌트 디자인 추가

--- frontend/project/src/components/Card/CustomCard.js
+++ frontend/project/src/components/Card/CustomCard.js
@@ -1,6 +1,50 @@
-import './css/CustomCard.module.min.css';
-const CustomCard = () => {
-  return <div className="card">This is CustomCard</div>;
+import styles from './CustomCard.module.scss';
+import { Container, Card, Button } from '@mui/material';
+const CustomCard = (props) => {
+  const info = {
+    name: '박찬용',
+    moeny: 15000,
+    bicycleNumber: 123456,
+    leftTime: 52
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Card
+        variant="outlined"
+        sx={{
+          borderColor: 'primary.main',
+          padding: '0px',
+          borderRadius: '5px'
+        }}
+        className={styles.card}
+      >
+        <div className={styles.cardHeader}>
+          <span>
+            <h1>{info.name}</h1> 회원님
+          </span>
+          <Button variant="contained" color="primary" sx={{ fontSize: '1rem' }}>
+            내 정보 관리 {`>`}
+          </Button>
+        </div>
+        <div className={styles.cardBody}>
+          <div className={styles.cardMoney}>
+            <div>
+              현재 보유중인 금액 <h3>{info.moeny}원</h3>
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ fontSize: '1rem', marginLeft: '2rem' }}
+            >
+              충전하기 {`>`}
+            </Button>
+          </div>
+          <div className={styles.cardInfo}>현재 대여중인 따릉이 정보</div>
+        </div>
+      </Card>
+    </Container>
+  );
 };
 
 export default CustomCard;
