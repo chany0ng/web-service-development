cf24d1636d97353146b142616c8542e166139bd5
ChanYong Park <krap5075@naver.com>
2023-11-23 02:48:01 +0900

    feat(TabBar): 탭바 기능 추가(라우팅제외)

--- frontend/project/src/components/TabBar/TabBar.js
+++ frontend/project/src/components/TabBar/TabBar.js
@@ -1,7 +1,47 @@
 import { useNavigate } from 'react-router-dom';
-import styles from './css/TabBar.module.min.css';
+import { useState, useEffect } from 'react';
+import styles from './TabBar.module.scss';
+import PersonIcon from '@mui/icons-material/Person';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import EditNoteIcon from '@mui/icons-material/EditNote';
 const TabBar = () => {
-  return <div>This is Tabbar.</div>;
+  const [selectedTab, setSelectedTab] = useState(1);
+  const tabClickHandler = (num) => {
+    if (selectedTab !== num) {
+      setSelectedTab(num);
+    }
+  };
+
+  return (
+    <div className={styles['flex-container']}>
+      <div
+        className={`${
+          selectedTab === 1 ? styles.activeTab : styles['flex-item']
+        }`}
+        onClick={() => tabClickHandler(1)}
+      >
+        <PersonIcon sx={{ fontSize: '25px' }} />
+        <div>회원정보 관리</div>
+      </div>
+      <div
+        className={`${
+          selectedTab === 2 ? styles.activeTab : styles['flex-item']
+        }`}
+        onClick={() => tabClickHandler(2)}
+      >
+        <MonetizationOnIcon sx={{ fontSize: '25px' }} /> <div> 결제 관리</div>
+      </div>
+      <div
+        className={`${
+          selectedTab === 3 ? styles.activeTab : styles['flex-item']
+        }`}
+        onClick={() => tabClickHandler(3)}
+      >
+        <EditNoteIcon sx={{ fontSize: '25px' }} />
+        <div> 이용정보 관리</div>
+      </div>
+    </div>
+  );
 };
 
 export default TabBar;
