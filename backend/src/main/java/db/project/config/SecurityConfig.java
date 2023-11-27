930ab0f8311c8e3dc6dbaaa3ca3eb4c226ecdf61
ChanYong Park <krap5075@naver.com>
2023-11-27 16:45:40 +0900

    Squashed commit of the following:
    commit 5383dceee55c9629fbe1eab84896b64039cd9235
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Mon Nov 27 04:22:38 2023 +0900
    
        리프레시 토큰 발급, 액세스 토큰 재발급 구현
    
    commit 4440735a898904013f096e7ca31684ccc5c3b84b
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Tue Nov 21 04:15:49 2023 +0900
    
        로그인 권한 쪽 수정
    
    commit d26274f205a9cd8e4ee721ad58898067d8f5f1ab
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Wed Nov 15 04:00:20 2023 +0900
    
        jwt 토큰 활용한 로그인 구현
    
    commit 61947bdbd18f0927596003271747c480f147f3d3
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Thu Nov 9 22:52:44 2023 +0900
    
        회원 가입 기능 구현
    
    commit c813fd93c9c188345df0cd3a164bdffc8d6d556e
    Author: minig0lem <92259521+minig0lem@users.noreply.github.com>
    Date:   Tue Nov 7 04:52:28 2023 +0900
    
        Delete .idea directory
    
    commit b07d36fde6b3f90fc2d9366a7d86acd06a4b57b0
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Tue Nov 7 04:48:56 2023 +0900
    
        억까
    
    commit 2fc332134b4d9cc2cffaf5cea46bb55b254f5afc
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Tue Nov 7 04:14:17 2023 +0900
    
        gitignore 변경
    
    commit 2885922c7b8851af2ab7ffecac88a68518fe3fd0
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Tue Nov 7 03:41:41 2023 +0900
    
        SecurityConfig 추가(2)
    
    commit 4652092ab41cc11c3ab4d0e6f92fd581fca555d3
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Tue Nov 7 03:40:12 2023 +0900
    
        Revert "SecurityConfig 추가"
    
        This reverts commit d0ed170da420c1d78539309cd88ddde29dab3208.
    
    commit 3180312cb9e73dbac13ab4cc60360989b6d237bc
    Author: minig0lem <92259521+minig0lem@users.noreply.github.com>
    Date:   Tue Nov 7 03:36:35 2023 +0900
    
        Delete .idea directory
    
    commit d0ed170da420c1d78539309cd88ddde29dab3208
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Tue Nov 7 03:33:51 2023 +0900
    
        SecurityConfig 추가
    
    commit ff935851fc13f4b1c26adf98d7a5ca5f665ca6b7
    Author: minig0lem <gmltjd0326@naver.com>
    Date:   Mon Nov 6 03:48:14 2023 +0900
    
        first commit
    
    commit d7362cc7866213ec1683aa470b6f2467f768fceb
    Author: ChanYong Park <91418544+chany0ng@users.noreply.github.com>
    Date:   Thu Nov 2 02:16:04 2023 +0900
    
        [Chany0ng] react 기본 세팅 (#1)
    
        * setting: 불필요한 파일 삭제/ 초기세팅
    
        * setting: 초기 파일 세팅
    
        * setting: eslint & prettier 세팅
    
        * setting: 기본 CRA lint사용 / prettier 설정확정

---
+++ backend/src/main/java/db/project/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package db.project.config;
+
+import db.project.config.jwt.TokenProvider;
+import db.project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http//.httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()))
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/signup", "/api/login", "/api/token").permitAll();
+                    auth.requestMatchers("/api/test").hasRole("ADMIN");
+                    auth.requestMatchers("/error/**").permitAll();
+                    auth.anyRequest().authenticated();
+                });
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.exceptionHandling(exception ->  {
+            exception.accessDeniedHandler(jwtAccessDeniedHandler);
+            exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+        });
+
+
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+}
---
+++ backend/src/main/java/db/project/domain/User.java
@@ -0,0 +1,82 @@
+package db.project.domain;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Builder
+public class User implements UserDetails {
+    private String id;
+    private String password;
+    private String email;
+    private String phone_number;
+    private int pw_question;
+    private String pw_answer;
+    private String role;
+    private int cash;
+    private int overfee;
+    private int ticket_id;
+
+
+//    @Builder
+//    public User(String id, String password, String email, String phone_number,
+//                int pw_question, String pw_answer, String role, int cash, int overfee, int ticket_id) {
+//        this.id = id;
+//        this.password = password;
+//        this.email = email;
+//        this.phone_number = phone_number;
+//        this.pw_question = pw_question;
+//        this.pw_answer = pw_answer;
+//        this.role = role;
+//        this.cash = cash;
+//        this.overfee = overfee;
+//        this.ticket_id = ticket_id;
+//    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role.toUpperCase().equals("ADMIN")) {
+            List<SimpleGrantedAuthority> list = new ArrayList<>();
+            list.add(new SimpleGrantedAuthority("ROLE_" + this.role.toUpperCase()));
+            list.add(new SimpleGrantedAuthority("ROLE_USER"));
+            return list;
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.toUpperCase()));
+    }
+
+    @Override
+    public String getUsername() {
+        return id;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
