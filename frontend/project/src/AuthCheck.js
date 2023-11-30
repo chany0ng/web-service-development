62ef0d6d9f87779270a9ce4d0d301d278d459bef
ChanYong Park <krap5075@naver.com>
2023-12-01 01:29:55 +0900

    refactor(AuthCheck): 페이지 로딩 전 인증 기능 수정

--- frontend/project/src/AuthCheck.js
+++ frontend/project/src/AuthCheck.js
@@ -1,28 +1,44 @@
 import { sendAccessToken } from './config';
 
 export const loginPageAuthCheck = async (navigation) => {
-  const accessToken = localStorage.getItem('accessToken');
-  if (accessToken) {
-    const { status } = await sendAccessToken(accessToken);
-    if (status === 401) {
-      navigation('/');
+  try {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      const { status } = await sendAccessToken(accessToken);
+      if (status === 401) {
+        alert('로그인 만료!');
+        navigation('/');
+      }
+      if (status === 200) {
+        alert('이미 로그인 상태입니다!');
+        navigation('/user/main');
+      }
     }
-    if (status === 200) {
-      navigation('/user/main');
-    }
+    alert('access token없는 접속!');
+  } catch (error) {
+    console.error(error);
+    alert(error);
   }
-  alert('access token없는 접속!');
 };
 
 export const mainPageAuthCheck = async (navigation) => {
-  const accessToken = localStorage.getItem('accessToken');
-  if (accessToken) {
-    const { status } = await sendAccessToken(accessToken);
-    if (status === 401) {
+  try {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      const { status } = await sendAccessToken(accessToken);
+      if (status === 401) {
+        alert('로그인 만료!');
+        navigation('/');
+      } else if (status === 403) {
+        alert('권한이 없습니다!');
+        navigation('/user/main');
+      }
+    } else {
+      alert('로그인이 필요합니다!');
       navigation('/');
     }
-  } else {
-    alert('로그인이 필요합니다!');
-    navigation('/');
+  } catch (error) {
+    console.error(error);
+    alert(error);
   }
 };
