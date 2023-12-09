bfc08bb3f21dfa3f34a70525c6ce7b37e938a9c6
ChanYong Park <krap5075@naver.com>
2023-12-10 06:28:06 +0900

    refactor(RentalRank): 더미데이터로 출력, 기능구현완료

--- frontend/project/src/pages/User/UserManage/UserManageRentalRanking.js
+++ frontend/project/src/pages/User/UserManage/UserManageRentalRanking.js
@@ -8,6 +8,7 @@ import styles from './UserManageRentalRanking.module.scss';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { mainPageAuthCheck } from '../../../AuthCheck';
+import { getFetch } from '../../../config';
 const outerTitle = ['회원정보 관리', '결제 관리', '이용정보 관리'];
 const innerTitle = ['대여/반납 이력', '대여소 랭킹'];
 const outerTab = 'manage';
@@ -18,59 +19,149 @@ const url = {
 };
 
 const head = ['등수', '아이디', '이용 거리', '이용 횟수'];
-const body = [
-  { name: '1', calories: 159, fat: 6.0, carbs: 24 },
-  {
-    name: '2',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37
-  },
-  { name: '3', calories: 262, fat: 16.0, carbs: 24 }
-];
-
+const body = {
+  user_ranking: 12345,
+  user_id: 'string',
+  user_duraiton_time: 'string',
+  rank: [
+    {
+      ranking: 1,
+      user_id: 'string1',
+      duration_time: 5
+    },
+    {
+      ranking: 2,
+      user_id: 'string2',
+      duration_time: 5
+    },
+    {
+      ranking: 3,
+      user_id: 'string3',
+      duration_time: 5
+    },
+    {
+      ranking: 4,
+      user_id: 'string4',
+      duration_time: 5
+    },
+    {
+      ranking: 5,
+      user_id: 'string5',
+      duration_time: 5
+    },
+    {
+      ranking: 6,
+      user_id: 'string6',
+      duration_time: 5
+    },
+    {
+      ranking: 7,
+      user_id: 'string7',
+      duration_time: 5
+    },
+    {
+      ranking: 8,
+      user_id: 'string8',
+      duration_time: 5
+    },
+    {
+      ranking: 9,
+      user_id: 'string9',
+      duration_time: 5
+    },
+    {
+      ranking: 10,
+      user_id: 'string10',
+      duration_time: 5
+    }
+  ]
+};
+const idData = body.rank.map((data) => {
+  return data.user_id;
+});
+const timeData = body.rank.map((data) => {
+  return data.duration_time;
+});
 const UserManageRentalRanking = () => {
   const navigate = useNavigate();
   useEffect(() => {
     mainPageAuthCheck(navigate);
+  }, []);
+  let timeRankId = [];
+  let timeRankData = [];
+  let countRankId = [];
+  let countRankData = [];
+  const getTimeRank = async () => {
+    try {
+      const response = await getFetch('api/rank/time');
+      if (response.status === 200) {
+        const data = await response.json();
+        timeRankId = data.rank.map((data) => {
+          return data.user_id;
+        });
+        timeRankData = data.rank.map((data) => {
+          return data.duration_time;
+        });
+      } else {
+        throw new Error('대여시간 랭킹 로딩 에러');
+      }
+    } catch (error) {
+      alert(error);
+      console.error(error);
+    }
+  };
+  const getCountRank = async () => {
+    try {
+      const response = await getFetch('api/rank/count');
+      if (response.status === 200) {
+        const data = await response.json();
+        countRankId = data.rank.map((data) => {
+          return data.user_id;
+        });
+        countRankData = data.rank.map((data) => {
+          return data.user_using_count;
+        });
+      } else {
+        throw new Error('대여횟수 랭킹 로딩 에러');
+      }
+    } catch (error) {
+      alert(error);
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getCountRank();
+    getTimeRank();
   }, []);
   return (
     <Layout>
       <TabBar title={outerTitle} select={outerTab} />
       <InnerTabBar title={innerTitle} select={innerTab} url={url} />
       <Article>
-        <h3 style={{ marginBottom: '10px' }}>나의 랭킹</h3>
+        <h3 style={{ marginBottom: '10px' }}>나의 대여 시간 랭킹</h3>
         <div className={styles.myRank}>
-          <span>- 등</span>
-          <span>park990328</span>
-          <span>2 시간</span>
+          <span>{timeRankData.user_ranking}등</span>
+          <span>{timeRankData.user_id}</span>
+          <span>{timeRankData.user_duraiton_time}</span>
         </div>
-        {/* useEffect에서 데이터 요청 후 받으면
-        사람:데이터, 형식일테니까 Object.메서드 사용해서
-        사람배열, 데이터배열 나눠서 저장 후 차트에 적용 */}
         <h2>대여 시간 순 차트</h2>
         <BarChart
           xAxis={[
             {
               scaleType: 'band',
-              data: [
-                '사람1',
-                '사람2',
-                '사람3',
-                '사람4',
-                '사람5',
-                '사람6',
-                '사람7',
-                '사람8',
-                '사람9',
-                '사람10'
-              ]
+              data: idData
             }
           ]}
-          series={[{ data: [20, 9, 8, 7, 6, 5, 4, 3, 2, 1] }]}
+          series={[{ data: timeData }]}
           height={300}
         />
         <br />
+        <h3 style={{ marginBottom: '10px' }}>나의 대여 횟수 랭킹</h3>
+        <div className={styles.myRank}>
+          <span>{countRankData.user_ranking}등</span>
+          <span>{countRankData.user_id}</span>
+          <span>{countRankData.user_using_count}</span>
+        </div>
         <h2>이용 횟수 순 차트</h2>
         <BarChart
           xAxis={[
@@ -93,8 +184,8 @@ const UserManageRentalRanking = () => {
           series={[{ data: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] }]}
           height={300}
         />
-        <h3 style={{ float: 'left' }}>전체 랭킹 목록(이용 거리 순)</h3>
-        <CustomTable headData={head} bodyData={body} />
+        {/* <h3 style={{ float: 'left' }}>전체 랭킹 목록(이용 거리 순)</h3>
+        <CustomTable headData={head} bodyData={body} /> */}
       </Article>
     </Layout>
   );
